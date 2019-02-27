43870d9391e650fba7d577d068c4a0ae9b4a632a
BostonianAdam <bostonianadam@gmail.com>
2019-02-27 03:44:49 -0500

    Added/updated site-js.js file
    -A new JS file was added for the scroll animation to the top of the page when the "back to top" icon is clicked.

--- js/site-js.js
+++ js/site-js.js
@@ -1,1 +1,8 @@
-
+//animate smooth scroll to top
+$('#top-link a').on('click', function(e) {
+    e.preventDefault();
+    const header = $('#header').position().top;
+    $('html, body').animate({
+        scrollTop: header
+    }, 900);
+});
