577d7667f9a60f0f4ac2e7293af29b4688468315
BostonianAdam <bostonianadam@gmail.com>
2019-09-06 12:35:34 -0400

    Sep 2019 v1.1 update

--- js/site-js.js
+++ js/site-js.js
@@ -1,5 +1,21 @@
+//show back to top icon when user scrolls past the window height
+var $topLink = $('.top-link');
+var $window = $(window);
+var winHeight = $window.height(); // Gets the window height
+winHeight = winHeight / 2;
+$window.on("scroll", function () {
+    if ($(this).scrollTop() > winHeight ) {
+        $topLink.addClass("showOn");
+    } else {
+        $topLink.removeClass("showOn");
+    }
+}).on("resize", function() {
+    // If the user resizes the window, get the height
+    winHeight = $(this).height();
+});
+
 //animate smooth scroll to top
-$('#top-link a').on('click', function(e) {
+$('.top-link a').on('click', function(e) {
     e.preventDefault();
     const header = $('#header').position().top;
     $('html, body').animate({
